30d7fd16810fce62f96883f7577faf4041fcc5f3
Melissa Lawrence <mlawrence537@gmail.com>
2021-03-09 15:40:36 -0500

    browse recipes

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -2,8 +2,21 @@ class Recipe < ActiveRecord::Base
 
     has_many :favorite_recipes
 
-    def self.all_categories
-        Recipe.all.map { |recipe| recipe.category }.uniq
+    def self.browse_all_helper
+       recipe_cats = Recipe.all.map { |recipe| recipe.category }.uniq
+       recipe_cats.each { |cat| puts cat }
+       puts "\nEnter a category name:"
+       answer = STDIN.gets.chomp
+       system 'clear'
+       Recipe.where(category: answer).each { |recipe| puts recipe.recipe_name }
+       puts "\nEnter a recipe name:"
+       answer = STDIN.gets.chomp
+       chosen_recipe = Recipe.all.find_by(recipe_name: answer)
+       system 'clear'
+       puts "#{chosen_recipe.recipe_name}:"
+       puts "\n#{chosen_recipe.ingredients}"
+       puts "\n#{chosen_recipe.prep}"
+       puts "\nYield : #{chosen_recipe.yield}"
     end
-    
+
 end
